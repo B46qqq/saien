a3229913190bffd6e76b2491294830cefa4b10b7
b46qqq <b46qqq@hotmail.com>
2019-05-21 23:26:02 +1000

    new .gitignore + some starter html

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup (
+    name = 'saien',
+    packages = ['saien'],
+    include_package_data = True,
+    install_requires =
+    [
+        'flask',
+    ],
+)
+
+    

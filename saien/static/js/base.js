90dd18bc099160fdb5c43ee3b5502fa94e27db61
b46qqq <b46qqq@hotmail.com>
2019-05-24 02:35:17 +1000

    basic base sidebar frontend design done, account activity needs to be consider later

---
+++ saien/static/js/base.js
@@ -0,0 +1,51 @@
+function get_toggle_btn(){
+    var toggle_btn = document.querySelector(".sidebar_toggle_switch");
+
+    if (toggle_btn == null){
+        toggle_btn = document.querySelector(".sidebar_toggle_switch_closed");
+        if (toggle_btn == null){
+            console.log("ERROR @ get_toggle_btn() > possibly classname not found");
+            return null;
+        }
+    }
+
+    return toggle_btn;
+}
+
+function toggle_animation(){
+    var toggle_btn = get_toggle_btn();
+    if (toggle_btn == null) return null;
+    
+    var tb_content = toggle_btn.textContent;
+    if (escape(tb_content) === "%u292B"){
+        var togglemation = document.querySelector(".sidebar_toggle_animation");
+        togglemation.innerHTML = "&#171;";
+    }
+        
+}
+
+
+function toggling(){
+    var toggle_btn = get_toggle_btn();
+    if (toggle_btn == null) return null;
+
+    var tb_content = toggle_btn.textContent;
+
+    var s = document.querySelector(".sidebar");
+    var sc = document.querySelector(".sidebar_container");
+    var stw = toggle_btn;
+
+    if (escape(tb_content) === "%u292B"){
+        s.style.width = "0px";
+        sc.style.left = "-185px";
+        stw.innerHTML = "&#9776;";
+        stw.removeAttribute("class");
+        stw.setAttribute("class", "sidebar_toggle_switch_closed");
+    } else { // Hope there are only two situation
+        s.style.width = "225px";
+        sc.style.left = "0px";
+        stw.innerHTML = "&#10539;";
+        stw.removeAttribute("class");
+        stw.setAttribute("class", "sidebar_toggle_switch");
+    }
+}

f2e25f307475070f724f86f4eb4674c5cd0b55f2
b46qqq <b46qqq@hotmail.com>
2019-05-30 00:24:28 +1000

    database half complete

--- saien/__init__.py
+++ saien/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_utils import database_exists
 from saien.config import Config
 
 
@@ -10,7 +9,7 @@ app.config.from_object(Config)
 
 # establish database and other instances here
 db = SQLAlchemy(app)
-
+db.create_all()
 # establish end
 
 from saien.level0.views import level0
